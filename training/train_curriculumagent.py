80498e40cd19f5ab0d7a398d6f50996350b84fc5
Oklyagina <kuuka2@bk.ru>
2026-09-05 14:50:43 +0100

    limited warnings

--- training/train_curriculumagent.py
+++ training/train_curriculumagent.py
@@ -4,6 +4,7 @@ The environment is either default from grid2op, or one of those available here: 
 """
 import logging
 import sys
+import warnings
 from pathlib import Path
 
 ROOT = Path(__file__).resolve().parents[1]
@@ -23,6 +24,37 @@ VERBOSE = False
 SHOW_PROGRESS = True
 LOG_LEVEL = logging.INFO if VERBOSE else logging.WARNING
 
+def configure_warnings() -> None:
+    warnings.filterwarnings(
+        "ignore",
+        message=r"You are using a legacy grid2op version, please upgrade grid2op\.",
+        category=UserWarning,
+        module=r"lightsim2grid\.lightSimBackend",
+    )
+    warnings.filterwarnings(
+        "ignore",
+        message=r"There were some Nan in the pp_net\.trafo\[\"tap_step_degree\"\], they have been replaced by 0",
+        category=UserWarning,
+    )
+    warnings.filterwarnings(
+        "ignore",
+        message=r"We found either some slack coefficient to be < 0\. or they were all 0\.We set them all to 1\.0 to avoid such issues",
+        category=UserWarning,
+        module=r"lightsim2grid\.gridmodel\.from_pandapower\._aux_add_slack",
+    )
+    for message in (
+        r"`UnifiedLogger` will be removed in Ray 2\.7\.",
+        r"The `JsonLogger interface is deprecated in favor of the `ray\.tune\.json\.JsonLoggerCallback` interface and will be removed in Ray 2\.7\.",
+        r"The `CSVLogger interface is deprecated in favor of the `ray\.tune\.csv\.CSVLoggerCallback` interface and will be removed in Ray 2\.7\.",
+        r"The `TBXLogger interface is deprecated in favor of the `ray\.tune\.tensorboardx\.TBXLoggerCallback` interface and will be removed in Ray 2\.7\.",
+    ):
+        warnings.filterwarnings(
+            "ignore",
+            message=message,
+            category=DeprecationWarning,
+            module=r"ray\..*",
+        )
+
 def configure_logging() -> None:
     logging.basicConfig(level=LOG_LEVEL, force=True)
     logging.getLogger().setLevel(LOG_LEVEL)
@@ -33,10 +65,12 @@ def shutdown_ray() -> None:
         ray.shutdown()
 
 configure_logging()
+configure_warnings()
 
 
 def main() -> None:
     configure_logging()
+    configure_warnings()
 
     loguru.logger.info("Making environment....")
     env = grid2op.make(str(ENV_DIR), backend=LightSimBackend())
